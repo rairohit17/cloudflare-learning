1bd5e61ff37e2d201c483fb1f9e57852dec5a396
rohitrai230393 <rohitrai170904@gmail.com>
2024-09-20 02:19:41 +0530

    cloudflare first worker deployement

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		console.log(request.method);
+		console.log(request.headers);
+		
+		if (request.method=="GET"){
+			return Response.json({
+				message:"you send a get request "
+			})
+		}
+		else return Response.json({
+			message:"you did not send a get request "
+		})
 	},
 } satisfies ExportedHandler<Env>;
